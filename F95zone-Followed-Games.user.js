7137634225a11903b946f7eda9e3c313005a4ce9
Len Anderson <Anderson.Len@outlook.com>
2023-04-21 23:44:39 +0200

    update to 1.7.0
    - add support for different nav on "latest" page
    - fix for changed ads in nav

--- F95zone-Followed-Games.user.js
+++ F95zone-Followed-Games.user.js
@@ -2,7 +2,7 @@
 // @name         F95zone - Followed Games
 // @namespace    https://github.com/LenAnderson/
 // @downloadURL  https://github.com/LenAnderson/F95zone-Followed-Games/raw/master/F95zone-Followed-Games.user.js
-// @version      1.6.2
+// @version      1.7.0
 // @author       LenAnderson
 // @match        https://f95zone.to/*
 // @grant        none
@@ -10,14 +10,14 @@
 
 
 
-(()=>{
+(async()=>{
 	'use strict';
 
 	const log = (...msgs)=>console.log.call(console.log, '[FFG]', ...msgs);
 	
 	
-	const $ = (query)=>document.querySelector(query);
-	const $$ = (query)=>Array.from(document.querySelectorAll(query));
+	const $ = (root,query)=>(query?root:document).querySelector(query?query:root);
+	const $$ = (root,query)=>Array.from((query?root:document).querySelectorAll(query?query:root));
 
 
 	const get = (url) => {
@@ -1588,54 +1588,64 @@ class GamesMonitor {
 
 
 
-	const navLink = $('[data-nav-id="ThePornDude"]'); {
-		navLink.href = '/followed-games';
-		navLink.target = '';
-		navLink.querySelector('span').textContent = 'Followed Games';
-	}
-	['Nutaku', 'LiveSexCams', 'LiveCamGirls'].forEach(it=>$(`[data-nav-id="${it}"]`).closest('li').remove());
-	if (location.href == 'https://f95zone.to/followed-games') {
-		navLink.closest('.p-navEl').classList.add('is-selected');
-		const app = new GamesMonitor();
-	} else if (location.href.search(/(https:\/\/f95zone.to\/threads\/)[^\/]+(\.\d+\/)/) == 0) {
-		log('add btn');
-		const watch = $('.rippleButton[data-sk-watch]');
-		if (watch) {
-			log('yup');
-			const url = location.href.replace(/(https:\/\/f95zone.to\/threads\/)[^\/]+(\.\d+\/).*$/, '$1$2');
-			let games = JSON.parse(localStorage.getItem('ffg-games') || '[]');
-			let followed = false;
-			if (games.filter(it=>it.url==url).length) {
-				followed = true;
-			}
-			const btn = document.createElement('a'); {
-				btn.classList.add('button--link');
-				btn.classList.add('button');
-				btn.classList.add('rippleButton');
-				btn.href = './follow';
-				btn.addEventListener('click', evt=>{
-					evt.preventDefault();
-					if (followed) {
-						games = games.filter(it=>it.url != url);
-						span.textContent = 'Follow';
-					} else {
-						games.push({url:url});
-						span.textContent = 'Unfollow';
+	const latest = $('[data-nav-id="LatestUpdates"]')?.closest('li');
+	if (latest) {
+		const navLink = latest.cloneNode(true); {
+			$(navLink, 'a').href = '/followed-games';
+			$(navLink, 'a > span').textContent = 'Followed Games';
+			latest.insertAdjacentElement('afterEnd', navLink);
+		}
+		$$('.p-navEl-link[target="_blank"]').forEach(it=>it.closest('li').remove());
+		if (location.href == 'https://f95zone.to/followed-games') {
+			$(navLink, '.p-navEl').classList.add('is-selected');
+			const app = new GamesMonitor();
+		} else if (location.href.search(/(https:\/\/f95zone.to\/threads\/)[^\/]+(\.\d+\/)/) == 0) {
+			log('add btn');
+			const watch = $('.rippleButton[data-sk-watch]');
+			if (watch) {
+				log('yup');
+				const url = location.href.replace(/(https:\/\/f95zone.to\/threads\/)[^\/]+(\.\d+\/).*$/, '$1$2');
+				let games = JSON.parse(localStorage.getItem('ffg-games') || '[]');
+				let followed = false;
+				if (games.filter(it=>it.url==url).length) {
+					followed = true;
+				}
+				const btn = document.createElement('a'); {
+					btn.classList.add('button--link');
+					btn.classList.add('button');
+					btn.classList.add('rippleButton');
+					btn.href = './follow';
+					btn.addEventListener('click', evt=>{
+						evt.preventDefault();
+						if (followed) {
+							games = games.filter(it=>it.url != url);
+							span.textContent = 'Follow';
+						} else {
+							games.push({url:url});
+							span.textContent = 'Unfollow';
+						}
+						localStorage.setItem('ffg-games', JSON.stringify(games));
+						followed = !followed;
+					});
+					const span = document.createElement('span'); {
+						span.classList.add('button-text');
+						span.textContent = followed ? 'Unfollow' : 'Follow';
+						btn.appendChild(span);
 					}
-					localStorage.setItem('ffg-games', JSON.stringify(games));
-					followed = !followed;
-				});
-				const span = document.createElement('span'); {
-					span.classList.add('button-text');
-					span.textContent = followed ? 'Unfollow' : 'Follow';
-					btn.appendChild(span);
+					const rip = document.createElement('div'); {
+						rip.classList.add('ripple-container');
+						btn.appendChild(rip);
+					}
+					watch.parentElement.appendChild(btn);
 				}
-				const rip = document.createElement('div'); {
-					rip.classList.add('ripple-container');
-					btn.appendChild(rip);
-				}
-				watch.parentElement.appendChild(btn);
 			}
+		}
+	} else {
+		const template = $('.p-navEl:not(.is-selected) > [data-nav-id]')?.closest('li');
+		const navLink = template.cloneNode(true); {
+			$(navLink, 'a').href = 'https://f95zone.to/followed-games';
+			$(navLink, 'a > span').textContent = 'Followed Games';
+			template.closest('ul').append(navLink);
 		}
 	}
 })();
